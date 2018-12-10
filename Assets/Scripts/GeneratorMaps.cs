1bcda052586b4dfbeba1f3ab30c9c2b817d61306
Vikamiojo <v.koldobanova@gmail.com>
2018-12-10 21:24:35 +0700

    Проект с проектом

---
+++ Assets/Scripts/GeneratorMaps.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneratorMaps : MonoBehaviour
+{
+    public GameObject map;
+    public GameObject[] prefabs;
+    public GameObject road;
+    public float radius;
+    GameObject mapItem;
+    Vector3 halpblala;
+    // Use this for initialization
+    void Start()
+    {
+        GenerateMap(20);
+        halpblala = new Vector3(20, 20, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void GenerateMap(int count)
+    {
+        for (int i = 0; i < prefabs.Length; i++)
+        {
+
+            for (int j = 0; j < count; j++)
+            {
+                Vector3 rndTransform = new Vector3(Random.Range(0, map.transform.localScale.x), 0, Random.Range(0, map.transform.localScale.z));
+                mapItem = Instantiate(prefabs[i], rndTransform, Quaternion.identity) as GameObject;
+                if ((Physics.OverlapBox(road.transform.localScale, mapItem.transform.position).Length > 0))
+                {
+                    DestroyImmediate(mapItem);
+                    count++;
+                }
+            }
+        }
+    }
+}
+
+
